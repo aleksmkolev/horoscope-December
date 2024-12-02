4f65bd67e0a6d94f9c34e3cf4cd083dd448a9ee8
Alexander Kolev <165174435+aleksmkolev@users.noreply.github.com>
2024-12-02 21:26:55 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function showImage(month) {
+    console.log('Attempting to show image for month:', month);
     var imagePopup = document.getElementById('image-popup');
     var image = imagePopup.querySelector('img');
+    image.onerror = function() {
+        console.error('Error loading image for month:', month);
+    };
+    image.onload = function() {
+        imagePopup.style.display = 'block';
+        setTimeout(function() {
+            imagePopup.style.opacity = 1;
+        }, 100);
+    };
     image.src = '/img/' + month + '.jpg';
-    imagePopup.style.display = 'block';
-    setTimeout(function() {
-        imagePopup.style.opacity = 1;
-    }, 600);
 }
 
-function hideImage() {
+function hidePopup() {
     var imagePopup = document.getElementById('image-popup');
     imagePopup.style.opacity = 0;
     setTimeout(function() {
